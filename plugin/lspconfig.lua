f37645a1aac59f13d27714f81e72b0045f479d79
Andjelko Cvjetkovic <andjelko.cvjetkovic@codepool.io>
2022-11-22 15:46:09 +0100

    fixup! typescript use relative import

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -67,7 +67,12 @@ nvim_lsp.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
-  capabilities = capabilities
+  capabilities = capabilities,
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = "non-relative"
+    }
+  }
 }
 
 nvim_lsp.sourcekit.setup {
